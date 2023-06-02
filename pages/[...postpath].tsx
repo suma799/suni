252dc64a9c39ee81edce9207073b63ed9618ed75
suma799 <133677909+suma799@users.noreply.github.com>
2023-06-02 19:50:47 +0530

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -4,7 +4,7 @@ import { GetServerSideProps } from 'next';
 import { GraphQLClient, gql } from 'graphql-request';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	const endpoint = "https://sofiyaflix.rf.gd/graphql"
+	const endpoint = "https://www.unique.lovestoblog.com/graphql"
 	const graphQLClient = new GraphQLClient(endpoint);
 	const referringURL = ctx.req.headers?.referer || null;
 	const pathArr = ctx.query.postpath as Array<string>;
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://sofiyaflix.rf.gd/` + encodeURI(path as string)
+					`https://www.unique.lovestoblog.com/` + encodeURI(path as string)
 				}`,
 			},
 		};
